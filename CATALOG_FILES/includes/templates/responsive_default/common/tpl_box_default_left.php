fe7d482a8a87f8e2b76d2cbd6143083bd7f7ed58
zcadditions <raymondabarbour@gmail.com>
2016-03-28 14:48:29 -0700

    Create tpl_box_default_left.php

---
+++ CATALOG_FILES/includes/templates/responsive_default/common/tpl_box_default_left.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Common Template - tpl_box_default_left.php
+ *
+ * @package templateSystem
+ * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: tpl_box_default_left.php 2975 2006-02-05 19:33:51Z birdbrain $
+ * @version ZCA/GIT: $Id: rbarbour New for v1.5.5 $
+ */
+
+// choose box images based on box position
+  if ($title_link) {
+    $title = '<a href="' . zen_href_link($title_link) . '">' . $title . BOX_HEADING_LINKS . '</a>';
+  }
+//
+?>
+<!--// bof: <?php echo $box_id; ?> //-->
+<!--<div class="leftBoxContainer" id="<?php echo str_replace('_', '-', $box_id ); ?>" style="width: <?php echo $column_width; ?>">-->
+
+<div class="leftBoxContainer <?php echo $desktopClass; ?> <?php echo $tabletClass; ?> <?php echo $mobileClass; ?>" id="<?php echo str_replace('_', '-', $box_id ); ?>">
+
+<h3 class="leftBoxHeading" id="<?php echo str_replace('_', '-', $box_id) . 'Heading'; ?>"><?php echo $title; ?></h3>
+<?php echo $content; ?>
+</div>
+<!--// eof: <?php echo $box_id; ?> //-->
+
