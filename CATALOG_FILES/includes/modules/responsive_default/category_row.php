13389cb8974f864d09466b111179c7372825d111
zcadditions <raymondabarbour@gmail.com>
2016-03-28 14:44:35 -0700

    Create category_row.php

---
+++ CATALOG_FILES/includes/modules/responsive_default/category_row.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * index category_row.php
+ *
+ * Prepares the content for displaying a category's sub-category listing in grid format.  
+ * Once the data is prepared, it calls the standard tpl_list_box_content template for display.
+ *
+ * @package page
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: category_row.php 4084 2006-08-06 23:59:36Z drbyte $
+ * @version ZCA/GIT: $Id: rbarbour New for v1.5.5 $
+ */
+if (!defined('IS_ADMIN_FLAG')) {
+  die('Illegal Access');
+}
+$title = '';
+$num_categories = $categories->RecordCount();
+
+$row = 0;
+$col = 0;
+$list_box_contents = '';
+if ($num_categories > 0) {
+  if ($num_categories < MAX_DISPLAY_CATEGORIES_PER_ROW || MAX_DISPLAY_CATEGORIES_PER_ROW == 0) {
+    $col_width = floor(100/$num_categories);
+  } else {
+    $col_width = floor(100/MAX_DISPLAY_CATEGORIES_PER_ROW);
+  }
+
+  while (!$categories->EOF) {
+    if (!$categories->fields['categories_image']) !$categories->fields['categories_image'] = 'pixel_trans.gif';
+    $cPath_new = zen_get_path($categories->fields['categories_id']);
+
+    // strip out 0_ from top level cats
+    $cPath_new = str_replace('=0_', '=', $cPath_new);
+
+    //    $categories->fields['products_name'] = zen_get_products_name($categories->fields['products_id']);
+
+// ZCAdditions.com, ZCA Responsive Components (BOF-edit 1 of 1)
+// $list_box_contents[$row][$col] = array('params' => 'class="categoryListBoxContents"' . ' ' . 'style="width:' . $col_width . '%;"',
+
+    $list_box_contents[$row][$col] = array('params' => 'class="categoryListBoxContents centeredContent columns' . MAX_DISPLAY_CATEGORIES_PER_ROW . '"',
+// ZCAdditions.com, ZCA Responsive Components (EOF-edit 1 of 1)
+
+                                           'text' => '<a href="' . zen_href_link(FILENAME_DEFAULT, $cPath_new) . '">' . zen_image(DIR_WS_IMAGES . $categories->fields['categories_image'], $categories->fields['categories_name'], SUBCATEGORY_IMAGE_WIDTH, SUBCATEGORY_IMAGE_HEIGHT) . '<br />' . $categories->fields['categories_name'] . '</a>');
+
+    $col ++;
+    if ($col > (MAX_DISPLAY_CATEGORIES_PER_ROW -1)) {
+      $col = 0;
+      $row ++;
+    }
+    $categories->MoveNext();
+  }
+}
+?>
